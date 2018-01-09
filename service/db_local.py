709b9408ac4b138846fed17e6ffe68c1d0fe0838
Victor Domingos <victordomingos@users.noreply.github.com>
2018-01-09 14:48:48 +0000

    Add files via upload

--- service/db_local.py
+++ service/db_local.py
@@ -5,72 +5,147 @@ Este módulo é parte integrante da aplicação Promais Service, desenvolvida po
 Victor Domingos e distribuída sob os termos da licença Creative Commons
 Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
 """
-
-
-from datetime import datetime
+#from datetime import datetime
 import os
-
-from sqlalchemy import (create_engine, Table, Column, Integer, String,
-                        ForeignKey, MetaData, DateTime)
+from sqlalchemy import create_engine
+from sqlalchemy import (Column, Integer, String,
+                        ForeignKey, MetaData, DateTime, func)
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy.orm import sessionmaker
 
 
 from global_setup import LOCAL_DATABASE_PATH
 
-metadata = MetaData()
-Base = declarative_base(metadata=metadata)
+Base = declarative_base()
 
+
+class Loja(Base):
+    __tablename__ = 'loja'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String)
+    utilizadores = relationship('Utilizador', secondary='loja_utilizador_link')
+    
+    def __repr__(self):
+        return f"<Loja(id={self.id}, Nome='{self.nome}', Utilizadores='{self.utilizadores}'>"
 
 
 class User(Base):
-    __tablename__ = 'users'
-    
-    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
     username = Column(String, index=True, nullable=False, unique=True)
-    email = Column(String, index=True, nullable=False, unique=True)
+    email = Column(String, index=True, nullable=False)
     password = Column(String, nullable=False, unique=False)
-    pwd_last_changed = Column(DateTime(), default=datetime.now)
-    created_on = Column(DateTime(), default=datetime.now)
-    updated_on = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
+    lojas = relationship(Loja, secondary='loja_utilizador_link')
+    pwd_last_changed = Column(DateTime(), default=func.now)
+    created_on = Column(DateTime(), default=func.now)
+    updated_on = Column(DateTime(), default=func.now, onupdate=func.now)
 
     def __repr__(self):
-        return f"<User(id={self.id}, name='{self.username}', email='{self.email}', password='{self.password}')>"
+        return f"<User(id={self.id}, name='{self.username}', email='{self.email}', password='{self.password}', loja={self.loja})>"
+
+
+class LojaUtilizadorLink(Base):
+    __tablename__ = 'loja_utilizador_link'
+    loja_id = Column(Integer, ForeignKey('loja.id'), primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+
+
+
+class Contact(Base):
+    __tablename__ = 'contact'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String, index=True, nullable=False, unique=True)
+    empresa = Column(String, index=True, nullable=False)
+    telefone = Column(String) 
+    telemovel = Column(String)  
+    telefone_empresa = Column(String) 
+    email = Column(String) 
+    morada = Column(String) 
+    cod_postal = Column(String) 
+    localidade = Column(String) 
+    pais = Column(String) 
+    nif = Column(String) 
+    notas = Column(String) 
+    is_cliente = Column(Integer) 
+    is_fornecedor = Column(Integer) 
+    is_loja = Column(Integer) 
+    criado_por_utilizador_id = Column(Integer, ForeignKey('user.id'))
+    criado_por_utilizador = relationship(User, backref=backref('users', uselist=True))
+    created_on = Column(DateTime(), default=func.now)
+    updated_on = Column(DateTime(), default=func.now, onupdate=func.now)
+
+    def __repr__(self):
+        return f"<Contact(id={self.id}, name='{self.nome}', empresa='{self.empresa}', is_cliente='{self.is_cliente}', is_fornecedor='{self.is_fornecedor}', is_loja='{self.is_loja}')>"
+
+
+class Artigo(Base):
+    __tablename__ = 'artigo'
+    id = Column(Integer, primary_key=True)
+    descr_artigo = Column(String, index=True, nullable=False)
+    part_number = Column(String, index=True)
+
+    def __repr__(self):
+        return f"<Article(id={self.id}, descr_artigo='{self.descr_artigo}', P/N='{self.part_number}'>"
+
+
+class Repair(Base):
+    __tablename__ = 'repair'
+    id = Column(Integer, primary_key=True)
+    cliente_id = Column(Integer, ForeignKey('contact.id'))
+    cliente = relationship(Contact, backref=backref('contacts', uselist=True))
+    artigo_id = Column(Integer, ForeignKey('artigo.id'))
+    artigo = relationship(Artigo, backref=backref('artigos', uselist=True))
+    sn = Column(String) 
+    fornecedor_id = Column(Integer, ForeignKey('contact.id'))
+    fornecedor = relationship(Contact, backref=backref('contacts', uselist=True))
+    estado_artigo = Column(Integer) 
+    obs_estado = Column(String) 
+    is_garantia = Column(Integer) 
+    data_compra = Column(DateTime())
+    num_fatura = Column(String) 
+    loja_compra = Column(String) 
+    desc_servico = Column(String) 
+    avaria_reprod_loja = Column(String) 
+    requer_copia_seg = Column(String) 
+    is_find_my_ativo = Column(Integer) 
+    Senha = Column(String) 
+    acessorios_entregues = Column(String) 
+    notas = Column(String) 
+    local_reparacao = Column(Integer) 
+    estado_reparacao = Column(String) 
+    fatura_fornecedor = Column(String) 
+    nar_autorizacao_rep  = Column(String) 
+    data_fatura_fornecedor  = Column(String) 
+    num_guia_rececao = Column(String) 
+    data_guia_rececao = Column(String) 
+    cod_resultado_reparacao = Column(String) 
+    descr_detalhe_reparacao = Column(String) 
+    novo_sn_artigo = Column(String) 
+    notas_entrega = Column(String) 
+    utilizador_entrega_id = Column(Integer, ForeignKey('user.id'))
+    utilizador_entrega = relationship(User, backref=backref('users', uselist=True))
+    data_entrega = Column(DateTime())     
+    num_quebra_stock = Column(String) 
+    is_stock = Column(Integer)
+    modo_entrega = Column(Integer) 
+    reincidencia_processo_id = Column(Integer) 
+    morada_entrega = Column(String) 
+    criado_por_utilizador_id = Column(Integer, ForeignKey('user.id'))
+    criado_por_utilizador = relationship(User, backref=backref('users', uselist=True))
+    created_on = Column(DateTime(), default=func.now)
+    updated_on = Column(DateTime(), default=func.now, onupdate=func.now)
+
+    def __repr__(self):
+        return f"<Repair(id={self.id})>"
 
 
 def init_database():
     engine = create_engine('sqlite:///'+os.path.expanduser(LOCAL_DATABASE_PATH))
-    metadata.create_all(engine)
-
-    """
-    repairs = Table('repairs', metadata,
-        Column('id', Integer, primary_key=True),
-        Column('cliente_id', Integer, ForeignKey("contacts.id"),
-        Column('cliente_id', Integer, ForeignKey("contacts.id"),
-        Column('email', String(255), index=True, nullable=False, unique=True),
-        Column('password', String(255), nullable=False, unique=False),
-        Column('pwd_last_changed', DateTime(), default=datetime.now),
-        Column('created_on', DateTime(), default=datetime.now),
-        Column('updated_on', DateTime(), default=datetime.now, onupdate=datetime.now),
-    )
-
-    repairs = Table('contacts', metadata,
-        Column('id', Integer, primary_key=True),
-        Column('cliente_id', Integer, ForeignKey("contacts.id"),
-        Column('email', String(255), index=True, nullable=False, unique=True),
-        Column('password', String(255), nullable=False, unique=False),
-        Column('pwd_last_changed', DateTime(), default=datetime.now),
-        Column('created_on', DateTime(), default=datetime.now),
-        Column('updated_on', DateTime(), default=datetime.now, onupdate=datetime.now),
-    )
-    """
-
-
-
-
-
+    session = sessionmaker()
+    session.configure(bind=engine)
+    Base.metadata.create_all(engine)
 
 
 def save_repair(rep_num):
@@ -169,22 +244,28 @@ def obter_lista_artigos_emprest():
     return artigos
 
 
-
-if __name__ == "__main__":    
-    # Testing...
-    init_database()
-    engine = create_engine('sqlite:///'+os.path.expanduser(LOCAL_DATABASE_PATH))
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
+def test_lojas():
+    l = Loja()
+    
+def test_users():
     for i in range(100):
-        new_user = User(username="Victor"+str(i), email="test@networkprojectforknowledge.org"+str(i), password="1234")
+        new_user = User(username="utilizador"+str(i), email="test@networkprojectforknowledge.org"+str(i), password="1234", loja=123)
         print(new_user)
         session.add(new_user)
         session.commit()
         print(">", new_user)
-    
 
     our_user = session.query(User).filter_by(username='Victor1').first()
     print("OUR USER:", our_user)
+
+
+if __name__ == "__main__":    
+    # Testing...
+    init_database()
+    #test_lojas()
+    #test_users()
+    
+    
+
+
     
