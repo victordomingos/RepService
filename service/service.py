62e7b84d53ef39c3280a5e7ee5b6a181f4cbf8f0
Victor Domingos <victordomingos@users.noreply.github.com>
2018-03-17 03:33:00 +0000

    Add files via upload

--- service/service.py
+++ service/service.py
@@ -14,12 +14,11 @@ muitas noites em claro, a partir de uma ideia original de Márcio Araújo.
 
 © 2018 Victor Domingos, Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
 """
-import tkinter as tk
 from tkinter import ttk
 
+from global_setup import *
 from gui.base_app import AppStatus
 from gui.main_window import App
-from global_setup import *
 
 
 if __name__ == "__main__":
@@ -28,7 +27,7 @@ if __name__ == "__main__":
     estado_app.janela_principal = App(root, estado_app)
     estilo_global = ttk.Style(root)
     estilo_global.theme_use(ESTILO_APP)
-    #estilo_global.theme_use("clam")
+    # estilo_global.theme_use("clam")
     root.configure(background='grey95')
     root.title('RepService')
     root.geometry(ROOT_GEOMETRIA)
